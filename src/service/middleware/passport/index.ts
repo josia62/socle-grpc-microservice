9d2dcf2562688c59862464120d5154a3d02a0f33
livai <livai@localhost.localdomain>
2024-10-24 14:45:44 +0300

    ok

--- src/service/middleware/passport/index.ts
+++
@@ -1,8 +0,0 @@
-import * as passport from "passport";
-
-import { frontLocalStrategy, jwtStrategy } from "./passport-local";
-import { passportStrategies } from "./passport-strategies";
-
-passport.use(jwtStrategy);
-passport.use(passportStrategies.frontLocal, frontLocalStrategy);
-export default passport;
--- src/service/middleware/passport/passport-strategies.ts
+++
@@ -1,5 +0,0 @@
-export const passportStrategies = {
-  jwt: "jwt",
-  local: "local",
-  frontLocal: "front-local",
-};
